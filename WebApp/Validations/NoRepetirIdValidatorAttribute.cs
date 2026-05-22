8097ac6f3cd6fd419bc3a231ffaa1552e31e069d
Jose Miguel Solis Garma <22470067@campeche.tecnm.mx>
2026-05-22 12:05:10 -0600

    Agrega validación personalizada

---
+++ WebApp/Validations/NoRepetirIdValidatorAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using WebApp.Models;
+
+
+namespace WebApp.Validations
+{
+    public class NoRepetirIdValidatorAttribute : ValidationAttribute
+    {
+        private IList<Proyector> _proyectores;
+        public NoRepetirIdValidatorAttribute(IList<Proyector> proyectores)
+        {
+            _proyectores = proyectores;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null) {
+                return null;
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
