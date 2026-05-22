79bf881fa1e5c38253b9541c843622e925d5328c
Jose Miguel Solis Garma <22470067@campeche.tecnm.mx>
2026-05-22 09:39:52 -0600

    Muestra la información de los proyectores utilizando una tabla HTML

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Xml;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -8,18 +9,64 @@ namespace WebApp.Controllers
     {
         public IActionResult Index()
         {
-            //Modelo de la vista
-            Proyector proyector = new()
-            {
-                Id = 1,
-                Marca = "Epson",
-                Modelo = "Ecotank",
-                NumeroDeSerie = "382901",
-                Situacion = SituacionProyector.Bueno,
-                FechaDeAlta = DateTime.Now
-            };
-            return View(proyector);
+            var modelo = LoadData();
+            return View(modelo);
         }
+
+        private IEnumerable<Proyector> LoadData()
+        {
+            var proyectores = new List<Proyector>();
+            {
+                proyectores.Add(new Proyector()
+                {
+                    Id = 1,
+                    Marca = "Epson",
+                    Modelo = "Ecotank",
+                    NumeroDeSerie = "382901",
+                    Situacion = SituacionProyector.Bueno,
+                    FechaDeAlta = DateTime.Now
+                });
+                proyectores.Add(new Proyector()
+                {
+                    Id = 2,
+                    Marca = "Epson",
+                    Modelo = "L3250",
+                    NumeroDeSerie = "758910",
+                    Situacion = SituacionProyector.Regular,
+                    FechaDeAlta = DateTime.Now
+                });
+                proyectores.Add(new Proyector()
+                {
+                    Id = 3,
+                    Marca = "Epson",
+                    Modelo = "Smart x50",
+                    NumeroDeSerie = "165289",
+                    Situacion = SituacionProyector.Bueno,
+                    FechaDeAlta = DateTime.Now
+                });
+                proyectores.Add(new Proyector()
+                {
+                    Id = 4,
+                    Marca = "Epson",
+                    Modelo = "I6270",
+                    NumeroDeSerie = "124503",
+                    Situacion = SituacionProyector.Malo,
+                    FechaDeAlta = DateTime.Now
+                });
+                proyectores.Add(new Proyector()
+                {
+                    Id = 5,
+                    Marca = "Epson",
+                    Modelo = "L5590",
+                    NumeroDeSerie = "456098",
+                    Situacion = SituacionProyector.Regular,
+                    FechaDeAlta = DateTime.Now
+                });
+            }
+            return proyectores;
+
+        }
+
 
         public IActionResult Privacy()
         {
